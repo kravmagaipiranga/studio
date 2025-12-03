3e34d71e6835476c4cf1db4d62e30b4f6129d114
Krav Magá Ipiranga <kravmagaipiranga@gmail.com>
2025-12-03 14:48:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-659171913-ed391",
+  "appId": "1:1055320484452:web:299c597a4c33393bcc8d71",
+  "apiKey": "AIzaSyCn_xw7AA8oQ2VoV6lJxFYenoaUfKSN56k",
+  "authDomain": "studio-659171913-ed391.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1055320484452"
+};
